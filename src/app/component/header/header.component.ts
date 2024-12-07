8e05650d955f9d516ea1aac2e311ae41cdf1eca2
somilsdiaz <somil.sandoval@icloud.com>
2024-12-06 20:08:36 -0500

    feat(HU0001): The responsive mode of the header has been adapted and improved

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';  // Importa el módulo necesario
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],  // Agrega MatIconModule aquí
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
