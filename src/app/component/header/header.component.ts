20fb38efd851a39168a63793edd802b615310ba2
somilsdiaz <somil.sandoval@icloud.com>
2024-12-07 16:29:20 -0500

    feat(HU0002): images about featured products have been added, also I was improvements in the design header

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,19 +1,23 @@
 import { Component, HostListener } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';  // Importa el módulo necesario
+import { MatIconModule } from '@angular/material/icon';  
+
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],  // Agrega MatIconModule aquí
+  imports: [MatIconModule], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   isScrolled = false;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.isScrolled = window.scrollY > 0; // Cambia el estado si el usuario se desplaza
+    this.isScrolled = window.scrollY > 0;
   }
 }
