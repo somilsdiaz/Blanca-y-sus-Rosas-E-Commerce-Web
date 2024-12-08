e6d30e7b44b039e21d59b1a275ca524e4bf500f0
somilsdiaz <somil.sandoval@icloud.com>
2024-12-07 20:51:07 -0500

    feat(HU0002): responsive design improved, specially buttoms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component';
 import {FeaturedProductsComponent} from "./component/featured-products/featured-products.component";
+import {ProductCardComponent} from "./component/product-card/product-card.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FeaturedProductsComponent],
+  imports: [RouterOutlet, HeaderComponent, FeaturedProductsComponent, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/component/featured-products/featured-products.component.ts
+++ src/app/component/featured-products/featured-products.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-featured-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductCardComponent], 
   templateUrl: './featured-products.component.html',
-  styleUrl: './featured-products.component.scss'
+  styleUrls: ['./featured-products.component.scss'] 
 })
-export class FeaturedProductsComponent {
-
-}
+export class FeaturedProductsComponent {}
