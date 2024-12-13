c37d7dc04000c6728ef037c843b5fb11197f0183
somilsdiaz <somil.sandoval@icloud.com>
2024-12-13 15:24:28 -0500

    feat(HU0004): styles of menu-responsive component has been improved

--- src/app/component/menu-reponsive/menu-reponsive.component.ts
+++ src/app/component/menu-reponsive/menu-reponsive.component.ts
@@ -5,8 +5,12 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './menu-reponsive.component.html',
-  styleUrls: ['./menu-reponsive.component.scss'] // Corregido: "styleUrls" en plural
+  styleUrls: ['./menu-reponsive.component.scss'] 
 })
 export class MenuReponsiveComponent {
   @Input() isMenuVisible: boolean = false; // Define el input para la visibilidad
+    // Lógica para cerrar el menú
+    closeMenu(): void {
+      this.isMenuVisible = false;
+    }
 }
